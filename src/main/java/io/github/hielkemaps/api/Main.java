f5bbee646b3f4b8288d9b3582ee3dd88c2424ebe
Hielke <hielkemaps@gmail.com>
2023-12-04 13:46:56 +0100

    HielkeAPI

---
+++ src/main/java/io/github/hielkemaps/api/Main.java
@@ -0,0 +1,13 @@
+package io.github.hielkemaps.api;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(new EventListener(), this);
+    }
+}
