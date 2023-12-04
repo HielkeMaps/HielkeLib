e0d3e7828b468845232bd5b839fb8a5e80f70293
Hielke <hielkemaps@gmail.com>
2023-12-04 14:04:42 +0100

    update

---
+++ src/main/java/io/github/hielkemaps/api/HielkeAPI.java
@@ -0,0 +1,10 @@
+package io.github.hielkemaps.api;
+
+import org.bukkit.Bukkit;
+
+public class HielkeAPI {
+
+    public static void Test(){
+        Bukkit.getLogger().info("TEST");
+    }
+}
--- src/main/java/io/github/hielkemaps/api/Main.java
+++ src/main/java/io/github/hielkemaps/api/Main.java
@@ -1,13 +1,14 @@
 package io.github.hielkemaps.api;
 
-import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
 
+    public static HielkeAPI API;
+
     @Override
     public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(new EventListener(), this);
+        API = new HielkeAPI();
     }
 }
